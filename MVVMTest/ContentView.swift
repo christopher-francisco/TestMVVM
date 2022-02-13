bac1aaedab40cf2bef02fde3a346a2dbc62e4333
Christopher Francisco <christopher.f.almanza@gmail.com>
2022-02-13 13:33:25 +0000

    Initial Commit

---
+++ MVVMTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVMTest
+//
+//  Created by Christopher Francisco on 13/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVMTest/MVVMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTestApp.swift
+//  MVVMTest
+//
+//  Created by Christopher Francisco on 13/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
